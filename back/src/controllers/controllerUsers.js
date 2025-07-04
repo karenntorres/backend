d3e113ce630e09e3f8203b5dab1078f4fcb2f19e
KarennTorres <kar33en@gmail.com>
2025-07-03 21:02:12 -0400

    trying changes

--- back/src/controllers/controllerUsers.js
+++ back/src/controllers/controllerUsers.js
@@ -1,5 +1,8 @@
 import bcrypt from 'bcryptjs';// para encriptar la contrasena
 import modelUsers from "../models/modelUsers.js"// para poder controlar el schema de usuarios
+import crypto from 'crypto';
+import nodemailer from 'nodemailer';
+import { text } from 'stream/consumers';
 
 const ControllerUsers = {
     createUser: async(sol , res)=>{
@@ -113,8 +116,55 @@ const ControllerUsers = {
     }
 };
 
+// funcion para crear una contrasena aleatoria
+function generarContrasenaAleatoria(){
+    return crypto.randomBytes(6).toString('hex');
+}
+
+export const forgotPasword = async (sol , res)=>{
+    try{
+        const { email }= sol.body
+        const user = await modelUsers.findOne({email});
+        if(!user){
+            return res.status(404).json({message:'No se encontro el correo registrado en la base de datos'});
+        }
+        const nuevaPassword = generarContrasenaAleatoria();
+        const hashedPassword = await bcrypt.hash(nuevaPassword , 10);
+
+        user.password = hashedPassword;
+        await user.save();
+
+        // configuracion del servicio de correo 
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth:{
+                user: process.env.EMAIL_NODE,
+                pass: process.env.PASS_NODE
+            },
+        });
+
+        // contenido del correo
+        const mailOptions = {
+            from: 'desarrollobitpruebas@gmail.com',
+            to: email,
+            subject: 'Recuperaciond e contraseña',
+            text: `Hola ${user.name}, \n\Tu nueva contraseña es: ${nuevaPassword}\n\ Recuerda actualizar tu contraseña en tu perfil por seguridad. \n\ Saludos desde el area de soporte.`
+        };
+
+        // envio de correo
+
+        await transporter.sendMail(mailOptions);
+
+        // Responder al usuario
+
+        res.status(200).json({message: 'Se ha enviado una nueva contraseña al correo registrado.'});
+
+    }catch(error){
+        console.error('Error al recuperar la contraseña', error);
+        res.status(500).json({message: ' Error interno en el servidor', error: error.message});
+    }
+}
+
+
+
 export default ControllerUsers;
-
-
-
-
--- back/src/routers/routerUsers.js
+++ back/src/routers/routerUsers.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import ControllerUsers from '../controllers/controllerUsers.js';
+import ControllerUsers, { forgotPasword } from '../controllers/controllerUsers.js';
 
 const routerUsers = Router();
 
@@ -8,7 +8,7 @@ routerUsers.get('/:id', ControllerUsers.readUser);
 routerUsers.get('/',ControllerUsers.readUsers);
 routerUsers.put('/:id',ControllerUsers.updateUser);
 routerUsers.delete('/:id', ControllerUsers.deleteUser);
+routerUsers.post('/forgot-password', forgotPasword);
 
 
 export default routerUsers;
-
